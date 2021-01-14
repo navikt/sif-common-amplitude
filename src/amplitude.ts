82a992bf774f9ad6a516057feaf837b1d031832d
Frode Hansen <frode.hansen@bekk.no>
2021-01-14 14:55:21 +0100

    Init

---
+++ src/amplitude.ts
@@ -0,0 +1,95 @@
+import { useEffect, useRef } from 'react';
+// import { getEnvironmentVariable } from '@navikt/sif-common-core/lib/utils/envUtils';
+import amplitude, { AmplitudeClient } from 'amplitude-js';
+import constate from 'constate';
+
+export enum AmplitudeEvents {
+    'sidevisning' = 'sidevisning',
+    'applikasjonStartet' = 'applikasjon-startet',
+    'søknadSendt' = 'skjema fullført',
+    'søknadFeilet' = 'skjemainnsending feilet',
+    'applikasjonInfo' = 'applikasjon-info',
+    'applikasjonHendelse' = 'applikasjon-hendelse',
+}
+
+export enum ApplikasjonHendelse {
+    'brukerSendesTilLoggInn' = 'brukerSendesTilLoggInn',
+    'vedleggOpplastingFeilet' = 'vedleggOpplastingFeilet',
+    'starterMedMellomlagring' = 'starterMedMellomlagring',
+    'avbryt' = 'avbryt',
+    'fortsettSenere' = 'fortsettSenere',
+}
+
+interface Props {
+    applicationKey: string;
+    logToConsoleOnly?: boolean;
+    isActive?: boolean;
+    children: React.ReactNode;
+}
+
+export const [AmplitudeProvider, useAmplitudeInstance] = constate((props: Props) => {
+    const { applicationKey, isActive, logToConsoleOnly } = props;
+    const instance = useRef<AmplitudeClient | undefined>();
+
+    useEffect(() => {
+        if (amplitude && isActive) {
+            instance.current = amplitude.getInstance();
+            if (instance.current) {
+                instance.current.init('default', '', {
+                    apiEndpoint: 'amplitude.nav.no/collect-auto',
+                    saveEvents: false,
+                    includeUtm: true,
+                    includeReferrer: true,
+                    platform: window.location.toString(),
+                });
+            }
+        }
+    }, [isActive]);
+
+    async function logEvent(eventName: string, eventProperties?: any) {
+        const eventProps = { ...eventProperties, app: applicationKey, applikasjon: applicationKey };
+        if (logToConsoleOnly) {
+            console.log({ eventName, eventProperties: eventProps });
+            return Promise.resolve();
+        }
+        if (instance.current) {
+            return instance.current.logEvent(eventName, eventProps);
+        }
+    }
+
+    function setUserProperties(properties: any) {
+        if (isActive && instance.current) {
+            instance.current.setUserProperties(properties);
+        }
+    }
+
+    async function logSidevisning(pageKey: string) {
+        return logEvent(AmplitudeEvents.sidevisning, {
+            pageKey,
+            team: 'sykdom-i-familien',
+        });
+    }
+
+    async function logSoknadSent(skjemanavn: string) {
+        return logEvent(AmplitudeEvents.søknadSendt, {
+            skjemanavn,
+            skjemaId: applicationKey,
+        });
+    }
+
+    async function logSoknadFailed(skjemanavn: string) {
+        return logEvent(AmplitudeEvents.søknadFeilet, {
+            skjemanavn,
+            skjemaId: applicationKey,
+        });
+    }
+
+    async function logHendelse(hendelse: ApplikasjonHendelse, details?: any) {
+        return logEvent(AmplitudeEvents.applikasjonHendelse, {
+            hendelse,
+            details,
+        });
+    }
+
+    return { logEvent, logSidevisning, setUserProperties, logSoknadSent, logSoknadFailed, logHendelse };
+});
